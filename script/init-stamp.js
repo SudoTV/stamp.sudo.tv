9a3a09bee8eb85fff35e7f1f25987ff21a3cbc50
WMXPY <wm@mengw.io>
2022-01-27 22:28:20 -0600

    improve init stamp script

--- script/init-stamp.js
+++ script/init-stamp.js
@@ -7,6 +7,7 @@ const Path = require("path");
 const Readline = require('readline');
 
 const blankImagePath = Path.join(__dirname, '..', 'stamp', '$', '_blank', 'blank.png');
+const blankYMLPath = Path.join(__dirname, '..', '_data', 'stamp', 'blank.yml');
 
 const readline = Readline.createInterface(
     process.stdin,
@@ -17,8 +18,31 @@ readline.question('[~~~~] Stamp Name: ', (stampName) => {
 
     const targetPathFolder = Path.join(__dirname, '..', 'stamp', '$', stampName);
 
-    FS.mkdirSync(targetPathFolder);
+    if (!FS.existsSync(targetPathFolder)) {
+        FS.mkdirSync(targetPathFolder);
+    }
+
     FS.copyFileSync(blankImagePath, Path.join(targetPathFolder, 'en-US.png'));
 
     console.log(`[INFO] Stamp Icon ${stampName} created.`);
+
+    const targetYMLPath = Path.join(__dirname, '..', '_data', 'stamp', `${stampName}.yml`);
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear().toString();
+    const currentMonth = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const currentDay = currentDate.getDate().toString().padStart(2, '0');
+
+    const blankYMLData = FS.readFileSync(blankYMLPath, 'utf8');
+    const fixedBlankYML = blankYMLData
+        .replace('identifier: blank', `identifier: ${stampName}`)
+        .replace('2022-01-27', `${currentYear}-${currentMonth}-${currentDay}`)
+        .replace('https://stamp.sudo.tv/stamp/$/_blank/blank.png', `https://stamp.sudo.tv/stamp/$/${stampName}/en-US.png`)
+        .replace('https://stamp.sudo.tv/stamp/$/_blank/blank.png', `https://stamp.sudo.tv/stamp/$/${stampName}/en-US.png`);
+
+    FS.writeFileSync(targetYMLPath, fixedBlankYML);
+
+    console.log(`[INFO] Stamp YML ${stampName} created.`);
+
+    readline.close();
 });
